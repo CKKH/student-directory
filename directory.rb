8051bf60e1c45f17356258647da9ba09e40979cf
Chris Hassan <chriskkhassan@gmail.com>
2018-11-14 12:07:17 +0000

    Added comments to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,10 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
 print "Overall, we have "
+# it's important that print() doesn't add new line characters here. If we
+# wanted new line characters after each print line, we'd use print "line1\n
+# line2\nline3", has to be in double quotes!
 print 11
 puts " great students"

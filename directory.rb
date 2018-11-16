3ffdc58d8a2b2844b426f24cc6acc098293d11dc
Chris Hassan <chriskkhassan@gmail.com>
2018-11-16 15:19:53 +0000

    code added to request and output additional student information

--- directory.rb
+++ directory.rb
@@ -1,18 +1,29 @@
 def input_students
   puts "Please enter the name of the student".center(175)
-  puts "To finish, just hit return twice".center(175)
+  # get the first set of student details
+  name = gets.chomp
+  puts "Please enter student's favourite programming language".center(175)
+  language = gets.chomp
+  puts "Please enter student's favourite hobby".center(175)
+  hobby = gets.chomp
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
   # while the name is not empty, repeart this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-#hobbies: :coding, country_of_birth: :UK, height: :height
+    students << {name: name, language: language, hobby: hobby, cohort: :november}
     puts "Now we have #{students.count} students".center(175)
-    #get another name from the user
+    # get next student details from the user unless no input entered
+    puts "Please enter the name of the next student, or to quit, enter no input".center(175)
     name = gets.chomp
+    if name.empty? == true
+      break
+    else
+      puts "Please enter student's favourite programming language".center(175)
+      language = gets.chomp
+      puts "Please enter student's favourite hobby".center(175)
+      hobby = gets.chomp
+    end
   end
   #return the array of input_students
   students
@@ -25,30 +36,9 @@ end
 
 def print_student_details(students)
   students.each.with_index(1) do |student, index|
-    if student[:name].length < 12
-      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(175)
-    else
-    end
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort), favourite programming language: #{student[:language]}, favourite hobby: #{student[:hobby]}".center(175)
   end
 end
-
-=begin
-each() print_student_details method using while loop & control flow
-def print_student_details(students)
-  index_position = 0
-  student_array = []
-  while index_position <= students.count do
-    student_array << students[index_position]
-    student_hash = student_array[index_position]
-    if student_hash == nil
-      break
-    else
-      puts "#{index_position + 1}. #{student_hash[:name]} (#{student_hash[:cohort]} cohort)"
-      index_position += 1
-    end
-  end
-end
-=end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students".center(175)
@@ -59,17 +49,3 @@ students = input_students
 print_header
 print_student_details(students)
 print_footer(students)
-
-=begin
-def request_student_info
-  puts "Please enter the name of the student"
-  name = gets.chomp
-  puts "Please enter their hobbies"
-  hobbies = gets.chomp
-  puts "Please enter their country of birth"
-  country_of_birth = gets.chomp
-  puts "Please enter height"
-  name = gets.chomp
-  puts "To finish, just hit return twice"
-end
-=end

391f3053b8f598760bbd78042686d89d7ac8b94f
Chris Hassan <chriskkhassan@gmail.com>
2018-11-19 11:58:46 +0000

    Add load_students method and code for loading student.csv information

--- directory.rb
+++ directory.rb
@@ -4,6 +4,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -22,6 +23,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -30,65 +33,65 @@ def process(selection)
 end
 
 def input_students
-  puts "Please enter the name of the next student, or press enter to finish".center(175)
+  puts "Please enter the name of the first student, or press enter to finish".center(115)
   name = gets.chomp
   # while name is not empty, repeat this code
   while !name.empty? do
-    puts "Please enter student's favourite programming language".center(175)
+    puts "Please enter student's favourite programming language".center(115)
     language = gets.chomp
-    puts "Please enter student's favourite hobby".center(175)
+    puts "Please enter student's favourite hobby".center(115)
     hobby = gets.chomp
     # list possible cohorts as symbols in array
     cohort_groups = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
-    puts "Please enter student's cohort".center(175)
+    puts "Please enter student's cohort".center(115)
     cohort = gets.chomp.capitalize.to_sym
     cohort = :Unknown unless cohort_groups.include?(cohort)
     # add the student hash to the array
     @students << {name: name, language: language, hobby: hobby, cohort: cohort}
     if @students.count == 1
-      puts "Now we have #{@students.count} student".center(175)
+      puts "Now we have #{@students.count} student".center(115)
     else
-      puts "Now we have #{@students.count} students".center(175)
+      puts "Now we have #{@students.count} students".center(115)
     end
-    # get next student details from the user unless quit entered
-    puts "Please enter the name of the next student, or press enter to finish".center(175)
+    # get next student details from the user or quits
+    puts "Please enter the name of the next student, or press enter to finish".center(115)
     name = gets.chomp
   end
 end
 
 def print_header
-  puts "The students of Villains Academy".center(175)
-  puts "-------------".center(175)
+  puts "The students of Villains Academy".center(115)
+  puts "-------------".center(115)
 end
 
 def print_student_details
   if !@students.empty?
     @students.each.with_index(1) do |student, index|
-      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort), favourite programming language: #{student[:language]}, favourite hobby: #{student[:hobby]}".center(175)
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort), favourite programming language: #{student[:language]}, favourite hobby: #{student[:hobby]}".center(115)
     end
   else
-    puts "No student information available".center(175)
+    puts "No student information available".center(115)
   end
  end
 
 def print_cohort_groups
   cohort_groups = @students.map { |student| student[:cohort] }.uniq
   cohort_groups.each do |cohort|
-    puts "#{cohort} cohort: ".center(175)
+    puts "#{cohort} cohort: ".center(115)
     @students.each do |student|
       if student[:cohort] == cohort
-        puts student[:name].center(175)
+        puts student[:name].center(115)
       end
     end
   end
 end
 
 def print_footer
-  puts "-------------".center(175)
+  puts "-------------".center(115)
   if @students.count == 1
-    puts "Overall, we have #{@students.count} great student".center(175)
+    puts "Overall, we have #{@students.count} great student".center(115)
   else
-    puts "Overall, we have #{@students.count} great students".center(175)
+    puts "Overall, we have #{@students.count} great students".center(115)
   end
 end
 
@@ -107,6 +110,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(", ")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end

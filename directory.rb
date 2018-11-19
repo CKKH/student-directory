77e41216341ea25e6c18397c9b8fa45396929135
Chris Hassan <chriskkhassan@gmail.com>
2018-11-19 18:15:49 +0000

    Refactored code, extracted request_student_name_or_exit_input_mode, get_student_info, and print_student_count methods from input_students

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-@students = [] # empty array accessible to all methods
+@students = []
 @name = ""
 @language = ""
 @hobby = ""
@@ -31,29 +31,18 @@ def process(selection)
 end
 
 def input_students
-  puts "Please enter the name of the first student, or press enter to finish".center(115)
-  @name = STDIN.gets.chomp
-  # while name is not empty, repeat this code
+  # gets first student name from user or allows them to exit input mode
+  request_student_name_or_exit_input_mode
+  # if name is provided, repeats this code
   while !@name.empty? do
-    puts "Please enter student's favourite programming language".center(115)
-    @language = STDIN.gets.chomp
-    puts "Please enter student's favourite hobby".center(115)
-    @hobby = STDIN.gets.chomp
-    # list possible cohorts as symbols in array
-    cohort_groups = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
-    puts "Please enter student's cohort".center(115)
-    @cohort = STDIN.gets.chomp.capitalize.to_sym
-    @cohort = :Unknown unless cohort_groups.include?(@cohort)
-    # add the student hash to the array
+    # gets student favourite programming language, hobby and cohort from user input
+    get_student_info
+    # add the student information as hash to the array
     build_student_list
-    if @students.count == 1
-      puts "Now we have #{@students.count} student".center(115)
-    else
-      puts "Now we have #{@students.count} students".center(115)
-    end
-    # get next student details from the user or quits
-    puts "Please enter the name of the next student, or press enter to finish".center(115)
-    @name = STDIN.gets.chomp
+    # print student count after each loop completes
+    print_student_count
+    # loop continues until user chooses to exit input mode
+    request_student_name_or_exit_input_mode
   end
 end
 
@@ -74,30 +63,49 @@ def print_student_details
     @students.each.with_index(1) do |student, index|
       puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort), favourite programming language: #{student[:language]}, favourite hobby: #{student[:hobby]}".center(115)
     end
-  else
-    puts "No student information available".center(115)
+  else puts "No student information available".center(115)
   end
- end
+end
 
 def print_cohort_groups
   cohort_groups = @students.map { |student| student[:cohort] }.uniq
   cohort_groups.each do |cohort|
     puts "#{cohort} cohort: ".center(115)
     @students.each do |student|
-      if student[:cohort] == cohort
-        puts student[:name].center(115)
-      end
+      puts student[:name].center(115) if student[:cohort] == cohort
     end
   end
 end
 
 def print_footer
   puts "-------------".center(115)
-  if @students.count == 1
-    puts "Overall, we have #{@students.count} great student".center(115)
-  else
-    puts "Overall, we have #{@students.count} great students".center(115)
-  end
+  puts print_student_count
+end
+
+def request_student_name_or_exit_input_mode
+  puts "Please enter student's name, or press enter to finish".center(115)
+  @name = STDIN.gets.chomp
+end
+
+def get_student_info
+  puts "Please enter student's favourite programming language".center(115)
+  @language = STDIN.gets.chomp
+  puts "Please enter student's favourite hobby".center(115)
+  @hobby = STDIN.gets.chomp
+  # list possible cohorts as symbols in array
+  cohort_groups = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+  puts "Please enter student's cohort".center(115)
+  @cohort = STDIN.gets.chomp.capitalize.to_sym
+  @cohort = :Unknown unless cohort_groups.include?(@cohort)
+end
+
+def build_student_list
+  @students << {name: @name, language: @language, hobby: @hobby, cohort: @cohort.to_sym}
+end
+
+def print_student_count
+  puts "We have #{@students.count} great student".center(115) if @students.count == 1
+  puts "We have #{@students.count} great students".center(115) if @students.count != 1
 end
 
 def save_students
@@ -131,10 +139,6 @@ def try_load_students
     puts "Sorry, #{filename} doesn't exist."
     exit
   end
-end
-
-def build_student_list
-  @students << {name: @name, language: @language, hobby: @hobby, cohort: @cohort.to_sym}
 end
 
 try_load_students
